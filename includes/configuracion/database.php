8399b28821b997aaf784e41ce74f4444201845fd
Julian Cappellari <juliancappellari11@gmail.com>
2024-06-05 22:58:07 -0300

    Configuracion bd

--- includes/configuracion/database.php
+++ includes/configuracion/database.php
@@ -1,6 +1,7 @@
 <?php
 function conectarDB() : mysqli{
-    $db = new mysqli('localhost', 'root', 'julian2003', 'bienesraices_crud');
+    // $db = new mysqli('localhost', 'root', 'julian2003', 'bienesraices_crud');
+    $db = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'));
     if (!$db){
         echo "No se pudo conectar con la base de datos";
         exit;
